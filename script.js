3c4fd0069189b62f29bf546983f85782efefef77
peti <petya.antal@gmail.com>
2025-01-08 14:19:35 +0100

    randomam

---
+++ script.js
@@ -0,0 +1,11 @@
+//generating button
+document.getElementById("generateBtn").onclick = handleClick;
+
+function handleClick(){
+    const values getInputValue(); 
+}
+
+function getInputValue(){
+    const inputElements = document.querySelectorAll("input")
+    
+}
---
+++ towo.md
@@ -0,0 +1,6 @@
+### Tennivalók ###
+- Gomb működtetése
+- Értékek megragadása, kinyerése (does it exsist)
+- Véletlen karakter generálás
+- jelszógenerátor
+- jelszavak megjelenítése(számozatlan lista)
